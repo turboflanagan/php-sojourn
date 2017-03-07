5a412ad5bb84c2feac54d6a097f9dad9fc077858
turboflanagan <turboflanagan@gmail.com>
2017-03-07 15:49:14 -0500

    init commit

--- Documents/Code/GreaterSum/katas/pokerHands/src/poker_hands.js
+++ Documents/Code/GreaterSum/katas/pokerHands/src/poker_hands.js
@@ -2,10 +2,16 @@
 var PokerHands = (function () {
     function PokerHands() {
     }
-    PokerHands.prototype.hand = function (hand) {
-        return hand;
+    PokerHands.prototype.pokerHand = function (dealtHand) {
+        function sortCardsLowToHigh(a, b) {
+            return a[0] - b[0];
+        }
+        var sortedHand = dealtHand.split(", ").sort(sortCardsLowToHigh);
+        console.log(sortedHand);
+        return sortedHand;
     };
     return PokerHands;
 }());
 exports.PokerHands = PokerHands;
+//
 //# sourceMappingURL=poker_hands.js.map
--- Documents/Code/GreaterSum/katas/pokerHands/src/poker_hands.ts
+++ Documents/Code/GreaterSum/katas/pokerHands/src/poker_hands.ts
@@ -1,7 +1,14 @@
 export class PokerHands {
-    hand(hand) {
-        return hand;
+    pokerHand(dealtHand) {
+        function sortCardsLowToHigh (a, b) {
+            return a[0] - b[0];
+        }
+        let sortedHand = dealtHand.split(", ").sort(sortCardsLowToHigh);
+        console.log(sortedHand);
+
+        return sortedHand;
+
     }
 }
 
-
+//
